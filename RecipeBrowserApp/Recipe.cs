9cd0358344a0be493b6522a7e55ebc8db917141b
Portalion <77tobik77@gmail.com>
2023-01-30 01:27:59 +0100

    Add Recipe Class

---
+++ RecipeBrowserApp/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace RecipeBrowserApp
+{
+    internal class Recipe
+    {
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+        public IEnumerable<string> Categories { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
